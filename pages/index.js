2d7edccf8c9640f18568b718203f2c1af6ce4fc5
Moiz-I <sky.moizi9@gmail.com>
2022-12-20 12:35:24 +0000

    final

---
+++ pages/index.js
@@ -0,0 +1,99 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+
+const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+  return (
+    <div className="root">
+      <div className="container">
+        <div className="header">
+          <div>
+            <Head>
+              <title>Rude-Bot🔥</title>
+              <meta
+                name="viewport"
+                content="initial-scale=1.0, width=device-width"
+              />
+            </Head>
+          </div>
+          <div className="header-title">
+            <h1>Rude-Bot🔥</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Tell the bot a bit about yourself..</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            placeholder="e.g. I am a computer science student and I like sleeping"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          {/* New code I added here */}
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        <a href="https://twitter.com/Moiz_zzz" target="_blank" rel="noreferrer">
+          <p className="self-promo">Made with 🌶️ by Moiz</p>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
